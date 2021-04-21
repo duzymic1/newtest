a9b39eebf157b6868115e71508ff1c8db2b1c20f
duzymic1 <michal.duzy@posti.com>
2021-04-21 12:25:43 +0000

    test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace newtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
